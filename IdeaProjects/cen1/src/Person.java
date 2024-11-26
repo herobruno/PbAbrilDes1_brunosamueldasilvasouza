e4190bef778d1ad016b931b8b23f03d138493031
herobruno <brunoss2118@gmail.com>
2024-11-26 14:41:34 -0300

    Parte 1: Definição da classe Person e métodos iniciais

---
+++ IdeaProjects/cen1/src/Person.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private Person spouse;
+    private List<Person> children;
+
+
+    public Person(String name) {
+        this.name = name;
+        this.children = new ArrayList<>();
+    }
+
+
+    public Person(String name, Person parent) {
+        this(name);
+        parent.addChild(this);
+    }
+
+
+
+    public void addChild(Person child) {
+        this.children.add(child);
+    }
+
+}
